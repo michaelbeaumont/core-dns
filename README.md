8b48527b08180fd587543926aaf812f3d5d6edcb
Mike Beaumont <mjboamail@gmail.com>
2023-08-20 04:48:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# core-dns
+
+This repo builds `coredns` with some extra plugins for libvirt VM
+and tailscale machine resolution.
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	_ "github.com/coredns/caddy/onevent"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/coremain"
+	_ "github.com/coredns/coredns/plugin/acl"
+	_ "github.com/coredns/coredns/plugin/any"
+	_ "github.com/coredns/coredns/plugin/bind"
+	_ "github.com/coredns/coredns/plugin/bufsize"
+	_ "github.com/coredns/coredns/plugin/cache"
+	_ "github.com/coredns/coredns/plugin/cancel"
+	_ "github.com/coredns/coredns/plugin/debug"
+	_ "github.com/coredns/coredns/plugin/dnssec"
+	_ "github.com/coredns/coredns/plugin/errors"
+	_ "github.com/coredns/coredns/plugin/forward"
+	_ "github.com/coredns/coredns/plugin/header"
+	_ "github.com/coredns/coredns/plugin/health"
+	_ "github.com/coredns/coredns/plugin/loadbalance"
+	_ "github.com/coredns/coredns/plugin/log"
+	_ "github.com/coredns/coredns/plugin/loop"
+	_ "github.com/coredns/coredns/plugin/metadata"
+	_ "github.com/coredns/coredns/plugin/metrics"
+	_ "github.com/coredns/coredns/plugin/minimal"
+	_ "github.com/coredns/coredns/plugin/nsid"
+	_ "github.com/coredns/coredns/plugin/ready"
+	_ "github.com/coredns/coredns/plugin/reload"
+	_ "github.com/coredns/coredns/plugin/rewrite"
+	_ "github.com/coredns/coredns/plugin/secondary"
+	_ "github.com/coredns/coredns/plugin/sign"
+	_ "github.com/coredns/coredns/plugin/timeouts"
+	_ "github.com/coredns/coredns/plugin/tls"
+	_ "github.com/coredns/coredns/plugin/tsig"
+	_ "github.com/coredns/coredns/plugin/view"
+	_ "github.com/damomurf/coredns-tailscale"
+	_ "github.com/michaelbeaumont/coredns-libvirt"
+)
+
+func main() {
+	dnsserver.Directives = []string{
+		"metadata",
+		"cancel",
+		"tls",
+		"timeouts",
+		"reload",
+		"nsid",
+		"bufsize",
+		"bind",
+		"debug",
+		"ready",
+		"health",
+		"prometheus",
+		"errors",
+		"log",
+		"acl",
+		"any",
+		"loadbalance",
+		"tsig",
+		"cache",
+		"rewrite",
+		"header",
+		"dnssec",
+		"minimal",
+		"secondary",
+		"loop",
+		"forward",
+		"on",
+		"sign",
+		"view",
+		"tailscale",
+		"libvirt",
+	}
+	coremain.Run()
+}
